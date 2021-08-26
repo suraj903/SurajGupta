eb458324964d277e97ae15db78859714395fac32
Suraj Gupta <48207614+suraj903@users.noreply.github.com>
2021-08-26 10:16:41 +0530

    first commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,61 @@
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+import Particles from 'react-particles-js';
+
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Learner", "Developer", "Adventurer"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <Particles
+    className="particles-canvas"
+    params={{
+      particles: {
+        number: {
+          value:50,
+          density: {
+            enable:true,
+            value_area:900
+          }
+        },
+        shape: {
+          type:"circle",
+          stroke:{
+            width:6,
+            color: "#f9ab00"
+          }
+        },
+        // color: "black"
+      }
+    }}
+    />
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="error" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Suraj Gupta</h1>
+          <h3>
+            Avid <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,47 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Namaste <i class="fas fa-praying-hands"></i>
+          </a>
+          <div className="itemContainer">
+            {/* <Person className="icon" /> */}
+            {/* <span>Home</span> */}
+            <a href="#intro">Home</a>
+          </div>
+          <div className="itemContainer">
+            {/* <Mail className="icon" /> */}
+            {/* <span>suraj.vnsgu@gmail.com</span> */}
+          </div>
+          <div className="itemContainer">
+            <a href="#portfolio">Portfolio</a>
+          </div>
+          <div className="itemContainer">
+            <a href="#works">Works</a>
+          </div>
+
+          <div className="itemContainer">
+            <a href="#testimonials">Testimonials</a>
+          </div>
+
+          <div className="itemContainer">
+            <a href="#contact">Contact</a>
+          </div>
+          
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
